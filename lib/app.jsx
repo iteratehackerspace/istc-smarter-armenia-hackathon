5c60081beb2ceee2a9c3b12bd160bf6e3e3d137a
Erik Davtyan <erikdavtyan@gmail.com>
2016-11-10 14:59:30 +0400

    header done

--- lib/app.jsx
+++ lib/app.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 
 const { Component } = React;
 
+
 const spot = max => Math.floor(Math.random() * max);
 
 const elementals = [
@@ -29,22 +30,45 @@ const elementals = [
 class Application extends Component {
   render () {
     //const Valodik = elementals[spot(elementals.length)].name;
+    const ledgerStyle = {
+      listItems: {
+            listStyleType: 'none',
+            color: 'white',
+            fontFamily: 'sans-serif',
+            margin: '0.5em auto',
+            padding: '.50rem',
+            wordWrap: 'break-word',
+            transition: 'top 1s',
+            width: '20%',
+            textAlign: 'center',
+          },
+      stylee: {
+           display: 'flex',
+           background: 'linear-gradient(to right, #000000, #28287A)',
+           scrollBehavior: 'smooth',
+           transition: 'ease-in 2s',
+           animationName: 'anim',
+           textAlign: 'center',
+           height: '3rem',
+           width: '100%',
+     },
+   };
     return (
       <div>
-        <div>
-          <li>
+        <div style = {ledgerStyle.stylee}>
+          <li style = {ledgerStyle.listItems}>
             Name
           </li>
-          <li>
+          <li style = {ledgerStyle.listItems}>
             Score
           </li>
-          <li>
+          <li style = {ledgerStyle.listItems}>
 	          Field
           </li>
-          <li>
+          <li style = {ledgerStyle.listItems}>
             Place
           </li>
-          <li>
+          <li style = {ledgerStyle.listItems}>
 	          Record
           </li>
         </div>
