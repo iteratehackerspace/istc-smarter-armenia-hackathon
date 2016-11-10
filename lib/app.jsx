1db3c1bb7de489cda4130cf3889080eb3a7ee6a4
Erik Davtyan <erikdavtyan@gmail.com>
2016-11-10 17:23:05 +0400

    moved to app.js

--- lib/app.jsx
+++ lib/app.jsx
@@ -31,18 +31,61 @@ class Application extends Component {
   }
   render () {
     const styles = {
-      divStyle: {
+      div1Style: {
         background: 'black',
+        width: '100%',
+      },
+      div2Style: {
+        background: 'black',
+        width: '100%',
+        height: '100px',
+        border: '1px solid white'
       },
       h1Style: {
         textAlign: 'center',
         color: 'white',
       },
+      listItems: {
+            listStyleType: 'none',
+            color: 'white',
+            fontFamily: 'sans-serif',
+            margin: '0.5em auto',
+            padding: '.50rem',
+            wordWrap: 'break-word',
+            transition: 'top 1s',
+            width: '20%',
+            textAlign: 'center',
+          },
+      stylee: {
+           display: 'flex',
+           background: 'linear-gradient(to right, #0080FF,#01A9DB,#DF7401 )',
+           //scrollBehavior: 'smooth',
+           transition: 'ease-in 2s',
+           //animationName: 'anim',
+           textAlign: 'center',
+           height: '3rem',
+           width: '100%',
+     },
     };
     return(
-      <div style = {styles.divStyle}>
-        <h1 style = {styles.h1Style}>Yerevan Works Out</h1>
-        <WorkoutSpots/>
+      <div style = {styles.div1Style}>
+          <h1 style = {styles.h1Style}>Yerevan Works Out</h1>
+          <div style = {styles.color}>
+            <div style = {styles.stylee}>
+              <li style = {styles.listItems}>
+                Place
+              </li>
+              <li style = {styles.listItems}>
+                Completed challenges today
+              </li>
+              <li style = {styles.listItems}>
+                Active challenges
+              </li>
+            </div>
+          </div>
+          <div style = {styles.div2Style}></div>
+          <div style = {styles.div2Style}></div>
+          <div style = {styles.div2Style}></div>
       </div>
     );
   };
