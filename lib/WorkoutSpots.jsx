cb52d656e9d70fa217149d5960157c58161b7694
Erik Davtyan <erikdavtyan@gmail.com>
2016-11-10 17:23:05 +0400

    moved to app.js

--- lib/WorkoutSpots.jsx
+++ lib/WorkoutSpots.jsx
@@ -10,45 +10,8 @@ class WorkoutSpots extends Component {
   }
   render () {
     const ledgerStyle = {
-      listItems: {
-            listStyleType: 'none',
-            color: 'white',
-            fontFamily: 'sans-serif',
-            margin: '0.5em auto',
-            padding: '.50rem',
-            wordWrap: 'break-word',
-            transition: 'top 1s',
-            width: '20%',
-            textAlign: 'center',
-          },
-      stylee: {
-           display: 'flex',
-           background: 'linear-gradient(to right, #0080FF,#01A9DB,#DF7401 )',
-           //scrollBehavior: 'smooth',
-           transition: 'ease-in 2s',
-           //animationName: 'anim',
-           textAlign: 'center',
-           height: '3rem',
-           width: '100%',
-     },
-     color: {
-       backgroundColor: 'green',
-     },
+
    };
-    return (
-      <div style = {ledgerStyle.color}>
-        <div style = {ledgerStyle.stylee}>
-          <li style = {ledgerStyle.listItems}>
-            Place
-          </li>
-          <li style = {ledgerStyle.listItems}>
-            Completed challenges today
-          </li>
-          <li style = {ledgerStyle.listItems}>
-            Active challenges
-          </li>
-        </div>
-      </div>
-    );
+    //return ();
 };
 }
