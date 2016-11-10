a453165399f173f45a8a927604d8ffa8787aa0ed
Erik Davtyan <erikdavtyan@gmail.com>
2016-11-10 13:54:47 +0400

    added pseudorandom array

--- lib/app.jsx
+++ lib/app.jsx
@@ -3,15 +3,35 @@ import { render } from 'react-dom';
 
 const { Component } = React;
 
+const pseudorandom = [
+  {
+    name: "Gohar",
+    id: Math.random(),
+    competition: "Running",
+    place: 1,
+  },
+  {
+    name: "Edgar",
+    id: Math.random(),
+    competition: "Geniosity",
+    place: 0,
+  },
+  {
+    name: "EdgarJ",
+    id: Math.random(),
+    competition: "nothing",
+    place: "last",
+  }
+];
+
 class Application extends Component {
   render () {
     return (
       <div>
-	Hello World
+  {pseudorandom[0].name}
       </div>
     );
   }
 };
-
 render(<Application/>,
        document.getElementById('container'));
